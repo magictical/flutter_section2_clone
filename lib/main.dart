756827ab47d279c5acd9cf7b7f4b96415e887e64
IL KWON KIM <magictical@gmail.com>
2019-12-23 12:23:24 +0900

    display transaction list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:section2_expenseapp_clone/widgets/new_transaction.dart';
 import './widgets/chart.dart';
 import './widgets/transaction_list.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +23,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly eateries ',
+      amount: 15.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Weekly eateries ',
+      amount: 15.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Weekly eateries ',
+      amount: 15.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Weekly eateries ',
+      amount: 15.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Weekly eateries ',
+      amount: 15.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +71,12 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         // chart
-        children: <Widget>[Chart(), TransactionList()],
+        children: <Widget>[
+          Chart(),
+          SingleChildScrollView(
+            child: TransactionList(_userTransactions),
+          )
+        ],
         // transaction_list
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
+  final List transactions;
+
+  TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        Container(
-          child: Text('each transaction'),
-        )
-      ],
+      children: transactions.map((transaction) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: Text(
+                  '\$ ${transaction.amount}',
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Text(transaction.title),
+                  Text(DateFormat('yyyy MM/DD').format(transaction.date))
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
